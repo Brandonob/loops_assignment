17647ffb0505a0807d04deef43a64c398d473c13
Brandon Brown <brandonb0206@gmail.com>
2019-09-30 21:56:16 -0400

    Problem 2

--- loopsassignment.js
+++ loopsassignment.js
@@ -1,7 +1,37 @@
 //Problem 1
+//a.
 
 let num = 65
 while(num >= 1){
     console.log(num)
     num--
+}
+
+//b.
+
+for(num = 20; num >= 1; num --){
+    console.log(num)
+}
+
+
+
+//Problem 2
+//a.
+
+let max = 20
+let low = 0
+while(low <= max){
+    if(low % 2 === 0){
+
+        console.log(low + " is even")  
+    }
+    low ++
+}
+
+//b.
+
+for(max = 0; max <= 20; max++ ){
+    if(low % 2 === 0){
+        console.log(max + " is even") 
+    }
 }
